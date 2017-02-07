563dd28cdccf8e9f5329198f3e24b01b2ce7f11f
juliapolbach <juliapolbach@gmail.com>
2017-02-07 08:52:55 +0100

    Júlia-Polbach

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,67 @@
+/* jshint esversion: 6*/
 class Elevator {
   constructor(){
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = '';
+    this.waitingList = [];
+    this.passengers = [];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
+  start() {
+    this.timer = setInterval(() => {this.update();}, 1000);
+
+  }
+
+  stop() {
+    clearInterval(this.timer);
+ }
+
+  update() {
+    this.log();
+    this.floorUp();
+    this.call();
+    if (this.waitingList.length !== 0) {
+      _passengersEnter();
+    }
+   }
+
+  _passengersEnter() {
+    this.passengers.concat(...this.waitingList);
+    this.waitingList.shift();
+    console.log("");
+  }
+
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp() {
+    if (this.floor < 10) {
+      this.direction = 'Up';
+      return this.floor += 1;
+    } else {
+      this.stop();
+    }
+  }
+
+  floorDown() {
+    if (this.floor > 0) {
+      this.direction = 'down';
+      return this.floor -= 1;
+    } else {
+      this.stop();
+    }
+  }
+
+  call(person) {
+    this.waitingList.push(person);
+    // this.requests.push(person.originFloor);
+  }
+
+  log() {
+    console.log('Direction :' + this.direction);
+    console.log('Floor :' + this.floor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,8 @@
+/* jshint esversion: 6*/
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+var _ = require('lodash');
+
+let elevator = new Elevator ();
+let person = new Person ('Sue', 2, 7);
+elevator.start();
